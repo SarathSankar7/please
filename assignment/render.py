05f4284c95cd794c44c33d4bfa8d0f90fef56c74
SarathSankar7 <sarathsankar1337@gmail.com>
2019-02-04 23:00:55 +0530

    first

--- assignment/render.py
+++ assignment/render.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
+app.config['SECRET-KEY'] =os.environ.get('SECRET_KEY','XYZ')
 @app.route('/')
 def home():
     return render_template('home.html')
